750b7318772458bd76aaf078b9fe9447095913ba
Iswarya <85299439+iswarya15@users.noreply.github.com>
2022-11-25 22:25:08 +0530

    Session 3 coding assignment (#3)
    * session 3 coding assignment
    
    * Session 3 theory assignment
    
    * Update README.md

---
+++ session 03/README.md
@@ -0,0 +1,26 @@
+### What is JSX?
+
+JSX is just syntactic sugar for `React.createElement`. It is neither HTML nor Javascript. Instead of calling React.createElement(component, props, children), we can use JSX to create React Elements with less work. When we write nested elements using createElement, code becomes less readable. Hence we use JSX. In the end, JSX is transpiled to React.createElement object only.
+
+### Superpowers of JSX
+
+- JSX improves _code readability_ and developing React Component using JSX is much easier than using React.createElement.
+- It also prevents Injection attacks like XSS - Cross side scripting by converting JSX into string before rendering.
+
+### Role of type attribute in script tag? What Options can I use there?
+
+The type attribute indicates the content type of the scripting language. A script tag having `type="module"` attribute tells that the script is a **Javascript module**. It may be importing other Javascript module(s) inside it. Importing modules cannot be done in normal JavaScript.
+
+```HTML
+<script src = "main.js" type = "text/javascript" ></script>
+
+<script src = "main.js" type = "application/javascript" ></script>
+
+<script src = "main.js" type = "application/ecmascript" ></script>
+```
+
+{TitleComponent} vs `{<TitleComponent/>}` & `{<TitleComponent></TitleComponent>}`
+
+{TitleComponent} => Functions are not valid as a React child. This may happen if you return a Component instead of <Component /> from render. Or maybe you meant to call this function rather than return it.
+
+`{<TitleComponent/>}` vs `{<TitleComponent></TitleComponent>}` renders the component
---
+++ session 03/src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import ReactLogo from "./assets/react-logo.png";
+import UserIcon from "./assets/user.png";
+
+const headerUsingCreateElement = React.createElement(
+  "div",
+  { className: "title" },
+  [
+    React.createElement("h1", {}, "Header using React.CreateElement"),
+    React.createElement("h2", {}, "React BootCamp"),
+    React.createElement("h3", {}, "In 3 months"),
+  ]
+);
+
+const course = "React BootCamp";
+
+const headerUsingJSX = (
+  <div className="title">
+    <h1>Header using JSX</h1>
+    <h2>{course}</h2>
+    <h3>In 3 months</h3>
+  </div>
+);
+
+const TitleComponent = () => <h1>Title Component</h1>;
+
+const HeaderUsingFuncComponent = () => {
+  return (
+    <div className="title">
+      <TitleComponent />
+      <h2>{course}</h2>
+      <h3>In 3 months</h3>
+    </div>
+  );
+};
+
+// {TitleComponent} => Functions are not valid as a React child. This may happen if you return a Component instead of <Component /> from render. Or maybe you meant to call this function rather than return it.
+
+// {<TitleComponent>} & {<TitleComponent></TitleComponent>} renders the component
+
+console.log("Header using JSX => ", headerUsingJSX);
+console.log("Header using FuncComponent => ", HeaderUsingFuncComponent());
+
+const SearchBar = () => {
+  return (
+    <div className="searchBar">
+      <input placeholder="Enter keyword here" />
+    </div>
+  );
+};
+
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <img src={ReactLogo} width="60px" height="60px" />
+      <SearchBar />
+      <img src={UserIcon} width="60px" height="60px" />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeaderComponent />);
