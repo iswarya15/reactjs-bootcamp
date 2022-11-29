c83d72f265c3d6937178ed06375d01606f77f9f6
iswarya <iswarya.ch99@gmail.com>
2022-11-29 14:16:54 +0530

    Session 04 coding assignment

---
+++ session 04/src/data.js
@@ -0,0 +1,92 @@
+export const teammates = [
+  {
+    name: "Vamshi",
+    company: "Cognizant",
+    location: "Hanumakonda",
+    experience: "2 years",
+    photo:
+      "https://cdn1.iconfinder.com/data/icons/information-technology-17/1700/9303_-_Programmer-256.png",
+    id: "Vamshigoud#4256",
+  },
+  {
+    name: "Smriti",
+    company: "Hughes systique corporation",
+    location: "Rajaganj",
+    experience: "3 years",
+    photo:
+      "https://cdn4.iconfinder.com/data/icons/coding-31/64/coder-g-programming-code-development-256.png",
+    id: "Smriti#9047",
+  },
+  {
+    name: "Shaksham",
+    company: "Yellow Ai",
+    location: "Gaya",
+    experience: "1 years",
+    photo:
+      "https://cdn1.iconfinder.com/data/icons/information-technology-17/1700/9303_-_Programmer-256.png",
+    id: "ShakshamSinha#6526",
+  },
+  {
+    name: "Shiva Kalyan",
+    company: "Virtusa",
+    location: "Dowleswaram",
+    experience: "2 years",
+    photo:
+      "https://cdn1.iconfinder.com/data/icons/information-technology-17/1700/9303_-_Programmer-256.png",
+    id: "kalyan#3314",
+  },
+  {
+    name: "Simran",
+    company: "Narula institute of technology",
+    location: "Bhagalpur",
+    experience: "7th Sem",
+    photo:
+      "https://cdn4.iconfinder.com/data/icons/coding-31/64/coder-g-programming-code-development-256.png",
+    id: "simran#6569",
+  },
+  {
+    name: "Piyush Nirmal",
+    company: "Henkel",
+    location: "NA",
+    experience: "7 years",
+    photo:
+      "https://cdn1.iconfinder.com/data/icons/information-technology-17/1700/9303_-_Programmer-256.png",
+    id: "laotzu#5238",
+  },
+  {
+    name: "Nitish Barik",
+    company: "Accenture",
+    location: "Cuttack",
+    experience: "5 years",
+    photo:
+      "https://cdn1.iconfinder.com/data/icons/information-technology-17/1700/9303_-_Programmer-256.png",
+    id: "Nitish Nivedan Barik",
+  },
+  {
+    name: "Iswarya Chelliah",
+    company: "Accenture",
+    location: "Chennai",
+    experience: "2 years",
+    photo:
+      "https://cdn4.iconfinder.com/data/icons/coding-31/64/coder-g-programming-code-development-256.png",
+    id: "Iswarya#0490",
+  },
+  {
+    name: "Jitendra Shekhawat",
+    company: "TCS",
+    location: "Jaipur",
+    experience: "3 years",
+    photo:
+      "https://cdn1.iconfinder.com/data/icons/information-technology-17/1700/9303_-_Programmer-256.png",
+    id: "J#2791",
+  },
+  {
+    name: "Sireesha",
+    company: "Apiwiz",
+    location: "Vijayawada",
+    experience: "0.5 years",
+    photo:
+      "https://cdn4.iconfinder.com/data/icons/coding-31/64/coder-g-programming-code-development-256.png",
+    id: "sireesha#2229",
+  },
+];
