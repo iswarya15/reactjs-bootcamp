b3ad8e987715157a6a4b4306e705c741d756301c
Iswarya <85299439+iswarya15@users.noreply.github.com>
2022-11-30 18:02:56 +0530

    Session 04 coding assignment (#4)
    * Session 04 coding assignment
    
    * added session 04 theory
    
    * removed unwanted curly braces

---
+++ session 04/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./style.css";
+import Header from "./Header";
+import Body from "./Body";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
