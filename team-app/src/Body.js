c7ad7afacbfbff8638246213a777d86b86f19464
Iswarya <85299439+iswarya15@users.noreply.github.com>
2022-12-07 13:20:33 +0530

    feat: implemented search (#5)
    * feat: implemented search
    
    * removed console
    
    * changed class name

---
+++ team-app/src/Body.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+import Card from "./Card";
+import { teammates } from "./data";
+import Search from "./Search";
+
+const Body = () => {
+  const [filteredCards, setFilteredCards] = useState(teammates);
+  return (
+    <>
+      <Search setFilteredCards={setFilteredCards} teammates={teammates} />
+      <div className="card-container">
+        {filteredCards.map((member) => {
+          return <Card key={member.id} member={member} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Body;
