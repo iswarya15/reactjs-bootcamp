a9094760fb3888f3fe5a7fa46304439b2e2b57a2
iswarya <iswarya.ch99@gmail.com>
2022-12-28 12:17:37 +0530

    fixed undefined error header component

--- team-app/src/components/header/Header.js
+++ team-app/src/components/header/Header.js
@@ -1,9 +1,11 @@
 import TeamLogo from "../../assets/coding.png";
 import "./header.css";
 
-export default Header = () => (
+const Header = () => (
   <div className="header">
     <img className="logo" src={TeamLogo} width="60" height="60" />
     <h1>Gladiators</h1>
   </div>
 );
+
+export default Header;
